8a97a57b2446ecd4b8ff7ef9c41160f514e9b5d2
serijun1214 <junki.seri@rakus-partners.co.jp>
2024-02-26 01:55:28 +0900

    ハッシュ化されたパスワードをデータベースに保存するように修正しました。

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +21,10 @@ public class AdministratorService {
 	@Autowired
 	private AdministratorRepository administratorRepository;
 
+	// パスワードをハッシュ化するためのインスタンスをDIコンテナから取得
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	/**
 	 * 管理者情報を登録します.
 	 * 
@@ -27,10 +32,10 @@ public class AdministratorService {
 	 */
 	public void insert(Administrator administrator) {
 
-		if(administratorRepository.findByMailAddress(administrator.getMailAddress()) != null) {
+		if (administratorRepository.findByMailAddress(administrator.getMailAddress()) != null) {
 			throw new IllegalArgumentException("そのメールアドレスは既に登録されています");
 		}
-		
+
 		administratorRepository.insert(administrator);
 	}
 
@@ -42,7 +47,12 @@ public class AdministratorService {
 	 * @return 管理者情報 存在しない場合はnullが返ります
 	 */
 	public Administrator login(String mailAddress, String password) {
-		Administrator administrator = administratorRepository.findByMailAddressAndPassward(mailAddress, password);
-		return administrator;
+		Administrator administrator = administratorRepository.findByMailAddress(mailAddress);
+
+		if (administrator != null && passwordEncoder.matches(password, administrator.getPassword())) {
+			return administrator;
+		}
+
+		return null;
 	}
 }
